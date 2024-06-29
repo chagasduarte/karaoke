e94e31edce467500743b066a9669624084e8d2ef
chagasduarte <chagasduarte.rdg@gmail.com>
2024-06-29 15:31:09 -0300

    Organizado cards e titulo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CardFilaComponent } from "../card-fila/card-fila.component";
+import { CardPessoasComponent } from '../card-pessoas/card-pessoas.component';
+import { CardPlayingComponent } from '../card-playing/card-playing.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [
+      CardFilaComponent,
+      CardPessoasComponent,
+      CardPlayingComponent
+    ]
 })
 export class HomeComponent {
 
